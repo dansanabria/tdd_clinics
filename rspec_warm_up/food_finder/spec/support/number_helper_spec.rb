d1096678cdb861bb7db4590ca3c2925477cba1bc
Daniel Sanabria <d.sanabria@kainos.com>
2015-12-21 11:20:26 +0000

    Adding specs for integer and float formats.

--- rspec_warm_up/food_finder/spec/support/number_helper_spec.rb
+++ rspec_warm_up/food_finder/spec/support/number_helper_spec.rb
@@ -3,39 +3,43 @@
 describe 'NumberHelper' do
 
   include NumberHelper
-  
+
   describe '#number_to_currency' do
-    
+
     context 'using default values' do
-      
-      it "correctly formats an integer"
-      
-      it "correctly formats a float"
+
+      it "correctly formats an integer" do
+        expect(number_to_currency(200)).to eq("$200.00")
+      end
+
+      it "correctly formats a float" do
+        expect(number_to_currency(1.75)).to eq("$1.75")
+      end
 
       it "correctly formats a string"
-      
+
       it "uses delimiters for very large numbers"
 
       it "does not have delimiters for small numbers"
 
     end
-    
+
     context 'using custom options' do
-      
+
       it 'allows changing the :unit'
 
       it 'allows changing the :precision'
-      
+
       it 'omits the separator if :precision is 0'
-      
+
       it 'allows changing the :delimiter'
-      
+
       it 'allows changing the :separator'
-            
+
       it 'correctly formats using multiple options'
-      
+
     end
-    
+
   end
-  
+
 end
