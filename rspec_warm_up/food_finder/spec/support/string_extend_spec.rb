dfec5d082a953ce0ad9ab5882f6d354baa76fe81
Daniel Sanabria <d.sanabria@kainos.com>
2015-12-21 16:43:19 +0000

    Specs for the Titleize method

--- rspec_warm_up/food_finder/spec/support/string_extend_spec.rb
+++ rspec_warm_up/food_finder/spec/support/string_extend_spec.rb
@@ -2,16 +2,24 @@ describe 'String' do
 
   describe "#titleize" do
 
-    it "capitalizes each word in a string"
-    
-    it "works with single-word strings"
-    
-    it "capitalizes all uppercase strings"
-    
-    it "capitalizes mixed-case strings"
-    
+    it "capitalizes each word in a string" do
+      expect("this is a string".titleize).to eq("This Is A String")
+    end
+
+    it "works with single-word strings" do
+      expect("something".titleize).to eq("Something")
+    end
+
+    it "capitalizes all uppercase strings" do
+      expect("SOMETHING IN ALL CAPS".titleize).to eq("Something In All Caps")
+    end
+
+    it "capitalizes mixed-case strings" do
+      expect("nOw SoMeThING LIKE this".titleize).to eq("Now Something Like This")
+    end
+
   end
-  
+
   describe '#blank?' do
 
     it "returns true if string is empty"
@@ -23,11 +31,11 @@ describe 'String' do
     # example: "\t\t\t"
 
     it "returns true if string contains only spaces and tabs"
-    
+
     it "returns false if string contains a letter"
 
     it "returns false if string contains a number"
-    
+
   end
-  
+
 end
